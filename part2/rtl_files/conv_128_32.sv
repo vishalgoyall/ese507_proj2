10dd77a90e1da2bcfff687b6f0a670513b999cc8
Prateek <prate93@gmail.com>
2019-10-26 19:07:00 -0400

    rtl updated for part2, and verified with part2 tb

---
+++ part2/rtl_files/conv_128_32.sv
@@ -0,0 +1,173 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ESE 507 : Project 2 (Convolution)
+// Authors : Prateek Jain and Vishal Goyal
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+module conv_128_32 #(parameter DATA_WIDTH_X = 8, parameter DATA_WIDTH_F = 8, parameter X_SIZE = 128, parameter F_SIZE = 32, parameter ACC_SIZE = 21) (
+	input clk, 
+	input reset, 
+	input s_valid_x, 
+	input s_valid_f, 
+	input m_ready_y,
+	input signed [DATA_WIDTH_X-1:0] s_data_in_x, 
+	input signed [DATA_WIDTH_F-1:0] s_data_in_f, 
+	output logic s_ready_f, 
+	output logic s_ready_x,
+	output logic m_valid_y, 
+	output logic signed [ACC_SIZE-1:0] m_data_out_y
+);
+
+//logic and parameter declarations
+parameter X_MEM_ADDR_WIDTH = $clog2(X_SIZE);  //bus width for x mem addr
+parameter F_MEM_ADDR_WIDTH = $clog2(F_SIZE);  //bus width for f mem addr
+parameter logic [F_MEM_ADDR_WIDTH-1:0] load_faddr_val = 0;
+
+logic xmem_full;
+logic xmem_addr_wr_ctrl;
+logic xmem_addr_rd_ctrl;
+logic [X_MEM_ADDR_WIDTH-1:0] xmem_addr;
+logic xmem_wr_en;
+logic xmem_reset;
+logic [X_MEM_ADDR_WIDTH-1:0] load_xaddr_val;
+logic signed [DATA_WIDTH_X-1:0] xmem_data;
+
+logic fmem_full;
+logic fmem_addr_wr_ctrl;
+logic fmem_addr_rd_ctrl;
+logic [F_MEM_ADDR_WIDTH-1 :0] fmem_addr;
+logic fmem_wr_en;
+logic fmem_reset;
+logic signed [DATA_WIDTH_F-1:0] fmem_data;
+
+logic conv_start, conv_pre_start;
+logic conv_done;
+
+logic signed [DATA_WIDTH_X+DATA_WIDTH_F-1:0] x_mult_f;
+logic signed [ACC_SIZE-1:0] accum_in;
+logic signed [ACC_SIZE-1:0] accum_out;
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Control Module to write data from Master into  X MEM using AXI
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  //Reset generation. 
+  //Conv_done is a one cycle pulse generated after convulation is complete
+  assign xmem_reset = reset || conv_done;   
+  
+  //ctrl module instantiation
+  ctrl_mem_write #(.MEM_ADDR_WIDTH(X_MEM_ADDR_WIDTH), .MEM_SIZE(X_SIZE)) ctrl_xmem_write_inst (
+	  .clk               (clk),  
+	  .reset             (xmem_reset),
+	  .s_valid           (s_valid_x),
+	  .s_ready           (s_ready_x),
+	  .mem_addr          (xmem_addr),
+	  .en_ext_ctrl       (conv_start),
+	  .ext_load_addr     (load_xaddr),
+	  .ext_load_addr_val (load_xaddr_val),
+	  .ext_incr_addr     (en_xaddr_incr),
+	  .mem_wr_en         (xmem_wr_en)
+  );
+
+  assign xmem_full = ~s_ready_x;
+  
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// X_MEM instantiation
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+  memory #(.WIDTH(DATA_WIDTH_X), .SIZE(X_SIZE),  .LOGSIZE(X_MEM_ADDR_WIDTH)) xmem_inst (
+          .clk        (clk),
+          .data_in    (s_data_in_x),
+          .data_out   (xmem_data),
+          .addr       (xmem_addr),
+          .wr_en      (xmem_wr_en)
+   );
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Control Module to write data from Master into  F_MEM using AXI
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  //Reset generation. 
+  //Conv_done is a one cycle pulse generated after convolution is complete
+  assign fmem_reset = reset || conv_done;   
+   
+  //ctrl module instantiation
+  ctrl_mem_write #(.MEM_ADDR_WIDTH(F_MEM_ADDR_WIDTH), .MEM_SIZE(F_SIZE)) ctrl_fmem_write_inst (
+	  .clk               (clk),  
+	  .reset             (fmem_reset),
+	  .s_valid           (s_valid_f),
+	  .s_ready           (s_ready_f),
+	  .mem_addr          (fmem_addr),
+	  .en_ext_ctrl       (conv_start),
+	  .ext_load_addr     (load_faddr),
+	  .ext_load_addr_val (load_faddr_val),
+	  .ext_incr_addr     (en_faddr_incr),
+	  .mem_wr_en         (fmem_wr_en)
+  );
+
+  assign fmem_full = ~s_ready_f;
+  
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// F_MEM instantiation
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+  memory #(.WIDTH(DATA_WIDTH_F), .SIZE(F_SIZE),  .LOGSIZE(F_MEM_ADDR_WIDTH)) fmem_inst (
+          .clk        (clk),
+          .data_in    (s_data_in_f),
+          .data_out   (fmem_data),
+          .addr       (fmem_addr),
+          .wr_en      (fmem_wr_en)
+   );
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Control Module for Convulation and AXI on output with master
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ 
+ always_ff @(posedge clk) begin
+	if (reset == 1'b1)
+		conv_pre_start <= 1'b0;
+	else 
+		conv_pre_start <= xmem_full && fmem_full;  //one cycle delay required to flush out X from memory during read start
+ end
+ assign conv_start = conv_pre_start && xmem_full && fmem_full;
+
+  ctrl_conv_output #(.F_MEM_SIZE(F_SIZE), .X_MEM_SIZE(X_SIZE), .X_MEM_ADDR_WIDTH(X_MEM_ADDR_WIDTH), .F_MEM_ADDR_WIDTH(F_MEM_ADDR_WIDTH))
+  ctrl_conv_output_inst (
+          .clk             (clk),
+	  .reset           (reset),
+	  .conv_start      (conv_start),
+	  .conv_done       (conv_done),
+	  .load_xaddr      (load_xaddr),
+	  .en_xaddr_incr   (en_xaddr_incr),
+	  .load_faddr      (load_faddr),
+	  .en_faddr_incr   (en_faddr_incr),
+	  .load_xaddr_val  (load_xaddr_val),
+	  .reset_accum     (reset_accum),
+	  .en_accum        (en_accum),
+	  .fmem_addr       (fmem_addr),
+	  .m_ready_y       (m_ready_y),
+	  .m_valid_y       (m_valid_y)
+  );
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// MAC unit of design
+// It uses signals coming from control convolution module to accumulate and reset
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// multiply xmem data with f mem data
+   assign x_mult_f = xmem_data*fmem_data;  
+
+// sign extending multiplier output to match adder dimensions
+   assign accum_in = accum_out + {{(ACC_SIZE-DATA_WIDTH_X-DATA_WIDTH_F){x_mult_f[$left(x_mult_f)]}} , x_mult_f};  
+
+   always_ff @(posedge clk) begin
+   	if (reset == 1'b1 || reset_accum == 1'b1) begin
+   		accum_out <= 0;
+   	end
+   	else if (en_accum == 1'b1) begin
+   		accum_out <= accum_in;
+   	end
+   end
+
+  assign m_data_out_y = accum_out;   //send output data from accumulator output
+
+
+endmodule
+
---
+++ part2/verif/part2_random_tb.sv
@@ -0,0 +1,181 @@
+
+// Peter Milder, 10/4/19
+// Testbench for ESE 507 Project 2 Part 2
+
+// This testbench does the following:
+//    - opens file random_in_f.hex and random_in_x.hex (input values to simulate) and stores 
+//      their values in arrays
+//    - opens expected_out.hex (expected output values) and stores its values in an array
+//    - on each clock cycle, randomly picks values for valid and ready control signals
+//    - uses these random control values to decide when to give new input and receive output
+//    - automatically check expected output is correct
+
+module tbench2();
+
+    parameter  NUMITS     = 1000, N = 128, M = 32;
+    localparam NUMXVALS   = N*NUMITS;
+    localparam NUMFVALS   = M*NUMITS;
+    localparam NUMOUTVALS = (N-M+1)*NUMITS;
+
+    logic clk, s_valid_x, s_valid_f, s_ready_x, s_ready_f, m_valid_y, m_ready_y, reset;
+    logic signed [7:0] s_data_in_x, s_data_in_f;
+    logic signed [20:0] m_data_out_y;
+
+    initial clk=0;
+    always #5 clk = ~clk;
+
+    // Instantiate DUT
+    conv_128_32 dut (.clk(clk), .reset(reset), 
+                  .s_data_in_x(s_data_in_x),   .s_valid_x(s_valid_x), .s_ready_x(s_ready_x),
+                  .s_data_in_f(s_data_in_f),   .s_valid_f(s_valid_f), .s_ready_f(s_ready_f),
+                  .m_data_out_y(m_data_out_y), .m_valid_y(m_valid_y), .m_ready_y(m_ready_y));
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    // code to feed some test inputs
+
+    // rb, rb2, and rb3 represent random bits. Each clock cycle, we will randomize the value of these bits.
+    // We will use rb to determine when to let our testbench send new x data. (When rb==0, we will not send valid data)
+    // We will use rb2 to determine when to let our testbench send new f data. (When rb2==0, we will not send valid data)
+    // We will use rb3 to determine when to let our testbench receive new y data. (When rb3==0, we will not receive results)
+    logic rb, rb2, rb3;
+    integer ignore;
+    always begin
+       @(posedge clk);
+       #1;
+       ignore = std::randomize(rb, rb2, rb3); // randomize rb
+    end
+
+
+    // Put our test data into these arrays. These are the values we will feed as input into the system.
+    logic signed [7:0] invals_x[NUMXVALS-1:0], invals_f[NUMFVALS-1:0];      
+    initial $readmemh("random_in_x.hex", invals_x); 
+    initial $readmemh("random_in_f.hex", invals_f); 
+
+    // Store the expected values in another array
+    logic signed [20:0] expectedOut[NUMOUTVALS-1:0];
+    initial $readmemh("expected_out.hex", expectedOut);
+        
+    logic [31:0] x_count;
+
+    // If our random bit rb is set to 1, and if x_count is within the range of our test vector (invals),
+    // we will set s_valid_x to 1.
+    always @* begin
+       if ((x_count>=0) && (x_count<NUMXVALS) && (rb==1'b1)) begin
+          s_valid_x=1;
+       end
+       else
+          s_valid_x=0;
+    end
+
+    // If s_valid_x is set to 1, we will put data on s_data_in_x.
+    // If s_valid_x is 0, we will put an X on the data_in to test that your system does not 
+    // process the invalid input.
+    always @* begin
+       if (s_valid_x == 1)
+          s_data_in_x = invals_x[x_count];
+       else
+          s_data_in_x = 'x;
+    end
+
+    // If we set s_valid_x and s_ready_x asserted on this clock edge, we will increment x_count just after
+    // this clock edge.
+    always @(posedge clk) begin
+       if (s_valid_x && s_ready_x)
+          x_count <= #1 x_count+1;
+    end
+  
+    ////////////////////////
+    // Setting values for input f
+
+    // Same logic but with f_count and s_data_in_f
+    
+    logic [31:0] f_count;
+    always @* begin
+       if ((f_count>=0) && (f_count<NUMFVALS) && (rb2==1'b1)) 
+          s_valid_f=1;
+       else
+          s_valid_f=0;
+    end
+
+    always @* begin
+       if (s_valid_f == 1)
+          s_data_in_f = invals_f[f_count];
+       else
+          s_data_in_f = 'x;
+    end
+
+    always @(posedge clk) begin
+       if (s_valid_f && s_ready_f)
+          f_count <= #1 f_count+1;
+    end
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+    // code to receive the output values
+
+
+    // we will use another random bit (rb3) to determine if we can assert m_ready_y.
+    logic [31:0] y_count;
+    always @* begin
+        if ((y_count >= 0) && (y_count < NUMOUTVALS) && (rb3==1'b1))
+            m_ready_y = 1;
+        else
+            m_ready_y = 0;
+    end
+   
+    integer errors=0;
+
+    always @(posedge clk) begin
+        if (m_ready_y && m_valid_y) begin
+            if (m_data_out_y !== expectedOut[y_count]) begin
+                $display("ERROR:   y[%d] = %d    expected output = %d", y_count, m_data_out_y, expectedOut[y_count]);
+                errors = errors+1;
+            end
+            y_count = y_count+1; 
+        end 
+    end
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    initial begin       
+        x_count=0; f_count=0; y_count=0;
+        
+        // Before first clock edge, initialize
+        m_ready_y = 0; 
+        reset = 0;
+
+        // reset
+        @(posedge clk); #1; reset = 1; 
+        @(posedge clk); #1; reset = 0;
+       
+        wait(y_count==NUMOUTVALS);
+
+        $display("\n------------- simulation finished ------------------");
+        $display("Simulated ", NUMITS, " iterations; ", NUMOUTVALS, " outputs");
+        if (errors > 0) 
+            $display("Detected ", errors, " errors");
+        else
+            $display("No errors detected");
+        $display("----------------------------------------------------\n");
+
+        
+        $finish;
+    end
+
+
+    // This is just here to keep the testbench from running forever in case of error.
+    // In other words, if your system never produces the expected outputs, this code will stop 
+    // the simulation after NUMITS*10000 clock cycles.
+    initial begin
+        repeat(NUMITS*10000) begin
+            @(posedge clk);
+        end
+        
+        $display("Warning: Output not produced within", NUMITS*10000, " clock cycles; stopping simulation so it doens't run forever");
+        $display("So far simulated ", y_count, " outputs; ", errors, " errors");
+        $stop;
+    end
+
+endmodule
+
