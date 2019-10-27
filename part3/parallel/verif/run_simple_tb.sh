7d1be083a6c801ce9c55642e203a56e5a89abd4d
Vishal Goyal <goyal.vishal1205@gmail.com>
2019-10-27 05:18:07 -0400

    Adding verification files

---
+++ part3/parallel/verif/run_simple_tb.sh
@@ -0,0 +1,14 @@
+rm -r work
+echo "++++++++++++ START COMPILATION ++++++++++"
+vlog -f ../rtl_file_list part1_simple_tb.sv
+echo "++++++++++++ COMPILATION DONE ++++++++++"
+
+echo "++++++++++++ Launch TestBench ++++++++++"
+if [ $1 = 'gui' ]; 
+then
+	vsim check_timing +acc
+else
+	vsim check_timing -c -do "run -all" 
+fi
+echo "++++++++++++ Exit TestBench ++++++++++"
+
